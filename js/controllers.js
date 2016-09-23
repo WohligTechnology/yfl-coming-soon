0e9d13275eeff9939cf2f6c2ebf9f7b2f3dc901e
ruchi <ruchiwohlig@gmail.com>
2016-09-23 19:28:50 +0530

    done with carousel

--- js/controllers.js
+++ js/controllers.js
@@ -12,105 +12,126 @@ angular.module('phonecatControllers', ['templateservicemod', 'navigationservice'
         'img/slider_food.jpg',
         'img/slider_love.jpg'
     ];
-
-    $scope.myInterval = 3000;
+    $scope.myInterval = 5000;
     $scope.noWrapSlides = false;
-    $scope.activeSlide = 0;
-    $scope.slides = [{
-        image: 'img/new/r6.jpg'
-    }, {
-        image: 'img/new/r6.jpg'
-    }, {
-        image: 'img/new/r6.jpg'
-    }, {
-        image: 'img/new/r6.jpg'
-    }];
-}
+    $scope.active = 0;
+    var slides = $scope.slides = [];
+    var currIndex = 0;
 
-$scope.teacher = [{
+    $scope.addSlide = function() {
+        var newWidth = 600 + slides.length + 1;
+        slides.push({
+            image: 'img/new/yoga-classes.jpg',
+            text: ['trending food recipes', 'trending food recipes', 'trending food recipes', 'trending food recipes'][slides.length % 4],
+            descp: 'Sed ut perspiciatis unde omnis perspiciatis unde omnis',
+            id: currIndex++
+        });
+    };
 
-        descp: "Sed ut perspiciatis unde omnis iste natexplicab."
-    }, {
 
-        descp: "Sed ut perspiciatis unde omnis iste natexplicab."
-    }, {
 
-        descp: "Sed ut perspiciatis unde omnis iste natexplicab."
+    for (var i = 0; i < 4; i++) {
+        $scope.addSlide();
     }
 
-];
 
-$scope.healthSlider = [{
-    img: "img/new/r6.jpg",
-    head: "HEALTHY FOOD FACTS",
-    descp: "Sed ut perspiciatis unde omnis iste natus error sit voluptat.Sed ut perspiciatis unde omnis iste natus error sit voluptat."
-}, {
-    img: "img/new/r6.jpg",
-    head: "HEALTHY FOOD FACTS",
-    descp: "Sed ut perspiciatis unde omnis iste natus error sit voluptat.Sed ut perspiciatis unde omnis iste natus error sit voluptat."
-}, {
-    img: "img/new/r6.jpg",
-    head: "HEALTHY FOOD FACTS",
-    descp: "Sed ut perspiciatis unde omnis iste natus error sit voluptat.Sed ut perspiciatis unde omnis iste natus error sit voluptat."
-}];
+    // http://stackoverflow.com/questions/962802#962890
+    function shuffle(array) {
+        var tmp, current, top = array.length;
 
-$scope.foodSlider = [{
-    img: "img/new/r6.jpg",
-    head: "Food",
-    descp: "Sed ut perspiciatis unde omnis iste natus error sit voluptat."
-}, {
-    img: "img/new/r6.jpg",
-    head: "Food",
-    descp: "Sed ut perspiciatis unde omnis iste natus error sit voluptat."
-}, {
-    img: "img/new/r6.jpg",
-    head: "Food",
-    descp: "Sed ut perspiciatis unde omnis iste natus error sit voluptat."
-}];
+        if (top) {
+            while (--top) {
+                current = Math.floor(Math.random() * (top + 1));
+                tmp = array[current];
+                array[current] = array[top];
+                array[top] = tmp;
+            }
+        }
 
-$scope.yogaclasses = [{
-    img: "img/new/yoga-classes.jpg",
-    head: "yoga classes",
-    descp: "Sed ut perspiciatis unde omnis iste natus error sit."
-}, {
-    img: "img/new/yoga-classes.jpg",
-    head: "yoga classes",
-    descp: "Sed ut perspiciatis unde omnis iste natus error sit."
-}, {
-    img: "img/new/yoga-classes.jpg",
-    head: "yoga classes",
-    descp: "Sed ut perspiciatis unde omnis iste natus error sit."
-}]; angular.element(document).ready(function() {
-    var feed = new Instafeed({
-        get: 'tagged',
-        tagName: 'yogafoodlove',
-        accessToken: '1388790889.e437b0c.e485f0e11e654dcdb5d92b3cc31c343d',
-        resolution: 'standard_resolution',
-        template: '<div class="col-xs-6 col-sm-4 margin-bot"><div class="relative"><img class="full-image"src="{{image}}"/><div class="caption"><div class="middle-margin">{{caption}}</div></div><div class="prod_info"><a href="https://www.facebook.com/dialog/share?app_id=956680054426963&display=popup&href={{link}}&redirect_uri=http://www.yogafoodlove.com"target="_blank"><i class=" trans fa fa-facebook"></i></a><a href="http://twitter.com/home?status={{link}}"target="_blank"><i class=" trans fa fa-twitter"></i></a><a href="http://pinterest.com/pin/create/button/?url={{link}}&media={{image}}"target="_blank"><i class=" trans fa fa-pinterest"></i></a><a href="{{link}}"target="_blank"><i class=" trans fa fa-instagram"></i></a></div></div></div>'
+        return array;
+    }
+
+    $scope.teacher = [{
+
+            descp: "Sed ut perspiciatis unde omnis iste natexplicab."
+        }, {
+
+            descp: "Sed ut perspiciatis unde omnis iste natexplicab."
+        }, {
+
+            descp: "Sed ut perspiciatis unde omnis iste natexplicab."
+        }
+
+    ];
+
+    // $scope.healthSlider = [{
+    //     img: "img/new/r6.jpg",
+    //     head: "HEALTHY FOOD FACTS",
+    //     descp: "Sed ut perspiciatis unde omnis iste natus error sit voluptat.Sed ut perspiciatis unde omnis iste natus error sit voluptat."
+    // }, {
+    //     img: "img/new/r6.jpg",
+    //     head: "HEALTHY FOOD FACTS",
+    //     descp: "Sed ut perspiciatis unde omnis iste natus error sit voluptat.Sed ut perspiciatis unde omnis iste natus error sit voluptat."
+    // }];
+
+    $scope.foodSlider = [{
+        img: "img/new/r6.jpg",
+        head: "Food",
+        descp: "Sed ut perspiciatis unde omnis iste natus error sit voluptat."
+    }, {
+        img: "img/new/r6.jpg",
+        head: "Food",
+        descp: "Sed ut perspiciatis unde omnis iste natus error sit voluptat."
+    }, {
+        img: "img/new/r6.jpg",
+        head: "Food",
+        descp: "Sed ut perspiciatis unde omnis iste natus error sit voluptat."
+    }];
+
+    $scope.yogaclasses = [{
+        img: "img/new/yoga-classes.jpg",
+        head: "yoga classes",
+        descp: "Sed ut perspiciatis unde omnis iste natus error sit."
+    }, {
+        img: "img/new/yoga-classes.jpg",
+        head: "yoga classes",
+        descp: "Sed ut perspiciatis unde omnis iste natus error sit."
+    }, {
+        img: "img/new/yoga-classes.jpg",
+        head: "yoga classes",
+        descp: "Sed ut perspiciatis unde omnis iste natus error sit."
+    }];
+    angular.element(document).ready(function() {
+        var feed = new Instafeed({
+            get: 'tagged',
+            tagName: 'yogafoodlove',
+            accessToken: '1388790889.e437b0c.e485f0e11e654dcdb5d92b3cc31c343d',
+            resolution: 'standard_resolution',
+            template: '<div class="col-xs-6 col-sm-4 margin-bot"><div class="relative"><img class="full-image"src="{{image}}"/><div class="caption"><div class="middle-margin">{{caption}}</div></div><div class="prod_info"><a href="https://www.facebook.com/dialog/share?app_id=956680054426963&display=popup&href={{link}}&redirect_uri=http://www.yogafoodlove.com"target="_blank"><i class=" trans fa fa-facebook"></i></a><a href="http://twitter.com/home?status={{link}}"target="_blank"><i class=" trans fa fa-twitter"></i></a><a href="http://pinterest.com/pin/create/button/?url={{link}}&media={{image}}"target="_blank"><i class=" trans fa fa-pinterest"></i></a><a href="{{link}}"target="_blank"><i class=" trans fa fa-instagram"></i></a></div></div></div>'
+        });
+        feed.run();
     });
-    feed.run();
-});
 
-setTimeout(function() {
-    (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s);
-        js.id = id;
-        js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.7";
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-    ! function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0],
-            p = /^http:/.test(d.location) ? 'http' : 'https';
-        if (!d.getElementById(id)) {
+    setTimeout(function() {
+        (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
             js = d.createElement(s);
             js.id = id;
-            js.src = p + "://platform.twitter.com/widgets.js";
+            js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.7";
             fjs.parentNode.insertBefore(js, fjs);
-        }
-    }(document, "script", "twitter-wjs");
-});
+        }(document, 'script', 'facebook-jssdk'));
+        ! function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0],
+                p = /^http:/.test(d.location) ? 'http' : 'https';
+            if (!d.getElementById(id)) {
+                js = d.createElement(s);
+                js.id = id;
+                js.src = p + "://platform.twitter.com/widgets.js";
+                fjs.parentNode.insertBefore(js, fjs);
+            }
+        }(document, "script", "twitter-wjs");
+    });
 
 })
 
