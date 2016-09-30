175623b61061bf97e2df34537d44711a08a790fa
Ruchiwohlig <ruchiwohlig@gmail.com>
2016-09-30 14:21:17 +0530

    done with restaurant detail page

--- js/controllers.js
+++ js/controllers.js
@@ -1713,38 +1713,47 @@ angular.module('phonecatControllers', ['templateservicemod', 'navigationservice'
         isFirstDisabled: false
     });
     $scope.rowdata = [{
-        img: "img/m1.jpg",
+        img: "img/teacher/m1.jpg",
         title: "Lorem Ipsum Dolor Sit Amet Consectetuer",
         data: "Colaba, Mumbai | Italian, Continental Cost for 2:  ₹ 500 -  ₹ 1000 | 9am - 6pm"
 
     }, {
-        img: "img/m1.jpg",
+        img: "img/teacher/m1.jpg",
         title: "Lorem Ipsum Dolor Sit Amet Consectetuer",
         data: "Colaba, Mumbai | Italian, Continental Cost for 2:  ₹ 500 -  ₹ 1000 | 9am - 6pm"
     }, {
-        img: "img/m1.jpg",
-        title: "Lorem Ipsum Dolor Sit Amet Consectetuer",
-        data: "Colaba, Mumbai | Italian, Continental Cost for 2:  ₹ 500 -  ₹ 1000 | 9am - 6pm"
-
-
-    }, {
-        img: "img/m1.jpg",
+        img: "img/teacher/m1.jpg",
         title: "Lorem Ipsum Dolor Sit Amet Consectetuer",
         data: "Colaba, Mumbai | Italian, Continental Cost for 2:  ₹ 500 -  ₹ 1000 | 9am - 6pm"
 
 
     }, {
-        img: "img/m1.jpg",
+        img: "img/teacher/m1.jpg",
         title: "Lorem Ipsum Dolor Sit Amet Consectetuer",
         data: "Colaba, Mumbai | Italian, Continental Cost for 2:  ₹ 500 -  ₹ 1000 | 9am - 6pm"
 
 
     }, {
-        img: "img/m1.jpg",
+        img: "img/teacher/m1.jpg",
+        title: "Lorem Ipsum Dolor Sit Amet Consectetuer",
+        data: "Colaba, Mumbai | Italian, Continental Cost for 2:  ₹ 500 -  ₹ 1000 | 9am - 6pm"
+
+
+    }, {
+        img: "img/teacher/m1.jpg",
         title: "Lorem Ipsum Dolor Sit Amet Consectetuer",
         data: "Colaba, Mumbai | Italian, Continental Cost for 2:  ₹ 500 -  ₹ 1000 | 9am - 6pm"
 
     }];
+
+    $scope.rate = 7;
+    $scope.max = 10;
+    $scope.isReadonly = false;
+
+    $scope.hoveringOver = function (value) {
+        $scope.overStar = value;
+        $scope.percent = 100 * (value / $scope.max);
+    }
 })
 
 
